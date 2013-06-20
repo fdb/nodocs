6ce4781284f56ee1b58154a0e6ac324e828935aa
Frederik De Bleser <frederik@burocrazy.com>
2013-06-20 14:31:27 +0200

    Add "server" command to Fabric.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,11 @@
-from fabric.api import env, run, cd
+from fabric.api import env, run, cd, local
 
 env.hosts = ['root@nodocs.nodebox.net']
+
+
+def server():
+    local('python nodocs.py')
+
 
 def update():
     nodebox_dir = '/www/nodocs.nodebox.net/nodebox'
@@ -8,12 +13,13 @@ def update():
         run('git pull')
         run('touch ../nodocs/nodocs.wsgi')
 
-def deploy():
 
+def deploy():
     project_dir = '/www/nodocs.nodebox.net/nodocs'
     with cd(project_dir):
         run('git pull')
         run('touch nodocs.wsgi')
+
 
 def all():
     update()
